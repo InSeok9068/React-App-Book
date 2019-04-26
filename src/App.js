bb04670a269fc876492dc2e2f4cb07d82b5c1d03
SANHAIT <dlstjr9068@naver.com>
2019-04-26 15:52:56 +0900

    컴포넌트 분리전

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { timeout } from 'q';
 
 const list = [
   {
@@ -19,19 +20,65 @@ const list = [
     objectID      : 1
   }
 ]
+
+
+// ES5
+// function isSearched(searchTerm){
+//   return function(item){
+//     return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+//   }
+// }
+
+// ES6
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props){
     super(props)
 
     this.state = {
       list,
+      searchTerm: '',
     }
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  onDismiss(id) {
+    // 1번
+    // const updatedList = this.state.list.filter(function isNotId(item){
+    //   return item.objectID !== id;
+    // })
+
+    // 2번
+    // function isNotId(item){
+    //   return item.objectID !== id;
+    // }
+    // const updatedList = this.state.list.filter(isNotId);
+
+    // 3번
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list : updatedList });
+  }
+
+  onSearchChange(event){
+    this.setState({ searchTerm : event.target.value });
   }
 
   render(){
+    const { searchTerm, list } = this.state;
     return(
       <div className="App">
-        {this.state.list.map(item =>
+        <form>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {list.filter(isSearched(searchTerm)).map(item =>
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
@@ -39,6 +86,13 @@ class App extends Component {
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
+              <spam>
+                <button onClick={() => this.onDismiss(item.objectID)}
+                  type="button"
+                >
+                  dismiss
+                </button>
+              </spam>
             </div>  
         )}
       </div>
