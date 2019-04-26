e8813c0aa37b0827581b411e6f70bc4beb9eb9f8
SANHAIT <dlstjr9068@naver.com>
2019-04-26 16:53:28 +0900

    2강 마무리

--- src/App.js
+++ src/App.js
@@ -70,11 +70,13 @@ class App extends Component {
   render(){
     const { searchTerm, list } = this.state;
     return(
-      <div className="App">
+      <div className="page">
         <Search 
           value={searchTerm}
           onChange={this.onSearchChange}
-        />
+        >
+          Search
+        </Search>
         <Table 
           list={list}
           pattern={searchTerm}
@@ -115,19 +117,36 @@ const Search = ({value, onChange, children}) => {
 
 class Table extends Component {
   render(){
+    const largeColumn = {
+      width : '40%'
+    }
+    const midColumn = {
+      width : '40%'
+    }
+    const smallColumn = {
+      width : '40%'
+    }
     const { list, pattern, onDismiss }  = this.props;
     return (
-      <div>
+      <div className="table">
         {list.filter(isSearched(pattern)).map(item =>
-          <div key={item.objectID}>
-            <span>
+          <div key={item.objectID} className="table-row">
+            <span style={largeColumn}>
               <a href={item.url}>{item.title}</a>
             </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <Button onClick={() => onDismiss(item.objectID)}>
+            <span style={{ width : '30%'}}>
+              {item.author}
+            </span>
+            <span style={{ width : '10%'}}>
+              {item.num_comments}
+            </span>
+            <span style={{ width : '10%'}}>
+              {item.points}
+            </span>
+            <span style={{ width : '10%'}}>
+              <Button onClick={() => onDismiss(item.objectID)}
+                className="button-button-inline"
+              >
                 Dismiss
               </Button>
             </span>
