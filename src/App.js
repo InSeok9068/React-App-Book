7fae5b07b4fc9d834c70c3cebecde4f1991a650e
SANHAIT <dlstjr9068@naver.com>
2019-04-29 11:22:14 +0900

    오류 처리

--- src/App.js
+++ src/App.js
@@ -54,6 +54,7 @@ class App extends Component {
       results: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error : null,
     }
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -107,7 +108,7 @@ class App extends Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+      .catch(error => this.setState({error}));
   }
 
   needsToSearchTopStories(searchTerm){
@@ -148,7 +149,8 @@ class App extends Component {
   render(){
     const { searchTerm, 
             results,
-            searchKey
+            searchKey,
+            error
     } = this.state;
 
     const page = (
@@ -174,10 +176,15 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table 
-        list={list}
-        onDismiss={this.onDismiss}
-        />
+        { error 
+          ? <div className="interactions">
+              <p>Something went wrong.</p>
+            </div>    
+          : <Table 
+          list={list}
+          onDismiss={this.onDismiss}
+          />
+        }
         <div className="interactions">
           <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
             More
